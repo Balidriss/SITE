e61d883480901587c8af679b4543f340d511421e
Balidriss <I.balijon@gmail.com>
2023-08-12 15:44:34 +0200

    starting

---
+++ Scripts/script.js
@@ -0,0 +1,46 @@
+console.log('bonjour');
+
+
+
+function changeLangue(lang) 
+{   
+    
+    fetch('JSON/contenu.json')
+    
+    .then(response => response.json())
+
+    
+    .then(data => 
+        { 
+            const elements = document.querySelectorAll('[contenu]');
+
+            elements.forEach(
+                element => 
+                { 
+                    const key = element.getAttribute('contenu');
+                   
+                if(data[lang] && data[lang][key])
+                {
+
+                element.style.display = 'block';
+                element.innerHTML= data[lang][key];
+
+                }
+
+            });
+
+        })
+    .catch(error => { console.error('probleme JSON introuvable')});
+
+
+}
+
+function detecteLangue()
+{
+    console.log(navigator.language);
+    
+    return  navigator.language != 'fr' ? 'en' : 'fr'; 
+}
+
+changeLangue(detecteLangue());
+    
