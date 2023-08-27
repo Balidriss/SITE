e7f62e59493314a1309c7cb72ad824090aba7e8c
Balidriss <I.balijon@gmail.com>
2023-08-27 15:16:34 +0200

    v1

--- Scripts/animatedbg.js
+++ Scripts/animatedbg.js
@@ -1,1 +1,9 @@
-container = document.getElementById("container");
+let bgAnimated = document.getElementById("bgAnime");
+let resolutionPixel = 1200; //a touché
+
+
+for (let i = 1; i <= resolutionPixel; i++) {
+    let pixel = document.createElement("span");
+    pixel.setAttribute("class", "pixel");
+    container.appendChild(pixel);
+}
