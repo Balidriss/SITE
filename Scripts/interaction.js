ca1031e6bdf54bc9ff5bd023860c47ba8ef73cb5
Balidriss <i.balijon@gmail.com>
2023-08-31 09:49:35 +0200

    nav interaction

--- Scripts/interaction.js
+++ Scripts/interaction.js
@@ -1,4 +1,7 @@
 function showSection(sectionClassName) {
-
+    const sections = document.getElementsByTagName("section");
+    for (const section of sections) {
+        section.style.display = "none";
+    }
     document.getElementById(sectionClassName).style.display = "block";
 }
