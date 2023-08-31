3b7c89769386ac273f69ac0d77565ce0990c0e98
Balidriss <i.balijon@gmail.com>
2023-08-31 11:23:01 +0200

    absolute

--- Scripts/changelangue.js
+++ Scripts/changelangue.js
@@ -1,30 +1,19 @@
 function changeLangue(lang) {
 
     fetch('JSON/contenu.json')
-
         .then(response => response.json())
-
-
         .then(data => {
             const elements = document.querySelectorAll('[contenu]');
-
             elements.forEach(
                 element => {
                     const key = element.getAttribute('contenu');
-
                     if (data[lang] && data[lang][key]) {
-
                         element.style.display = 'inline';
                         element.innerHTML = data[lang][key];
-
                     }
-
                 });
-
         })
         .catch(error => { console.error('probleme JSON introuvable') });
-
-
 }
 
 
